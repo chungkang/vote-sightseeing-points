8faca31af143103c5987e86d30b26a9d5b9a876e
chungkang <chungkang.c@gmail.com>
2019-07-08 22:08:46 +0900

    create project

---
+++ src/main/java/com/chungkang/webservice/service/PostsService.java
@@ -0,0 +1,29 @@
+package com.chungkang.webservice.service;
+
+import com.chungkang.webservice.domain.posts.PostsRepository;
+import com.chungkang.webservice.dto.posts.PostsMainResponseDto;
+import com.chungkang.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
