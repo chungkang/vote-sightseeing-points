943119134435fcdbbf6ca3b67edd8d5cda29984a
chungkang <chungkang.c@gmail.com>
2019-07-27 21:19:34 +0900

    fiexed

--- src/main/java/com/chungkang/webservice/web/WebRestController.java
+++ src/main/java/com/chungkang/webservice/web/WebRestController.java
@@ -32,7 +32,7 @@ public class WebRestController {
     public String getProfile () {
         return Arrays.stream(env.getActiveProfiles())
                 .filter(e -> "set1".equals(e) || "set2".equals(e) || "local".equals(e))
-                        .findFirst()
-                        .orElse("");
+                .findFirst()
+                .orElse("");
     }
 }
