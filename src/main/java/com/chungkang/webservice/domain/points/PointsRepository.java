e16259b009ccc27c85cfc476e9e298176cc29632
chungkang <chungkang.c@gmail.com>
2019-08-19 22:51:22 +0900

    version 0.2.1 deploy

--- src/main/java/com/chungkang/webservice/domain/points/PointsRepository.java
+++ src/main/java/com/chungkang/webservice/domain/points/PointsRepository.java
@@ -1,19 +1,15 @@
 package com.chungkang.webservice.domain.points;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Map;
 import java.util.stream.Stream;
 
 public interface PointsRepository extends JpaRepository<Points, Long>{
 
-    @Query("SELECT p FROM Points p ")
+    @Query("SELECT p FROM Points p ORDER BY p.modifiedDate DESC ")
     Stream<Points> findAllPoints();
 
-    @Query("SELECT p FROM Points p WHERE p.permit='Y'")
+    @Query("SELECT p FROM Points p WHERE p.permit='Y' ORDER BY p.point_no DESC")
     Stream<Points> findByPermit();
 
     @Query("SELECT count(p) FROM Points p WHERE p.permit='Y'")
